d1c07b55fe6fa6519e24705216c291d176b9cd18
talg <tal.grynbaum@market.com>
2020-06-23 18:14:34 +0300

    with shop page, collection preview and collection item components

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './collection-preview.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
+
+const CollectionPreview = ({title, items}) => (
+    <div className='collection-preview'>
+        <h1 className='title'>{title}</h1>
+        <div className="preview">
+            {items
+                .filter((item, idx) => idx < 4)
+                .map(({id, ...otherCollectionProps}) => (
+                <CollectionItem key={id} {...otherCollectionProps}/>
+            ))}
+        </div>
+    </div>
+)
+
+export default CollectionPreview
