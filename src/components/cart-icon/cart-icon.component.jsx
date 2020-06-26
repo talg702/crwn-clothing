001acce006e6408db0f634db808886c910ec8116
talg <tal.grynbaum@market.com>
2020-06-26 13:43:16 +0300

    cart icon & cart dropdown

---
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import { connect } from 'react-redux';
+import './cart-icon.styles.scss';
+import { toggleCartHidden } from '../../redux/cart/cart.action';
+
+const CartIcon = ({ toggleCartHidden }) => {
+    console.log(toggleCartHidden)
+    return (
+    <div className="cart-icon" onClick={toggleCartHidden}>
+        <ShoppingIcon className="shopping-icon"/>
+        <span className="item-count">0</span>
+    </div>
+    )}
+const mapDispatchToProps = dispatch => ({
+    toggleCartHidden: () => {
+        console.log(toggleCartHidden);
+        return dispatch(toggleCartHidden());
+    }
+})
+
+export default connect(null, mapDispatchToProps)(CartIcon);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,8 +5,10 @@ import { connect} from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crwn.svg';
 
 import './header.styles.scss';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className="logo"/>
@@ -24,11 +26,16 @@ const Header = ({ currentUser }) => (
                 : 
                 <Link className="option" to="/signin">SIGN IN</Link>
             }
+            <CartIcon />
         </div>
+        {
+            hidden ? null : <CartDropDown />
+        }
     </div>
 )
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({  user: { currentUser }, cart: { hidden } })  => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
